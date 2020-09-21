5cbb64a7f47099ce34725c8fb9a1db365536ab8e
Rychillie <rychillie@hotmail.com>
2020-09-21 20:40:04 -0300

    Added google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,5 +37,12 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`roboto mono`, `muli\:400,400i,700,700i`],
+        display: "swap",
+      },
+    },
   ],
 }
