29d2c975ee8236d2191a930a6b10a1562e00e0c6
Rychillie <rychillie@hotmail.com>
2020-09-22 22:30:09 -0300

    Add global styles

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,24 @@
 import React from "react"
-import { ThemeProvider } from "styled-components"
+import { createGlobalStyle, ThemeProvider } from "styled-components"
 import Theme from "./src/themes/theme"
 
+const GlobalStyles = createGlobalStyle`
+    * {
+        box-sizing: border;
+        margin: 0;
+        padding: 0;
+    }
+
+    body, html {
+        font-family: ${props => props.theme.fonts.main};
+        height: 100%;
+        background-color: ${props => props.theme.colors.light1}
+    }
+`
+
 export const wrapRootElement = ({ element }) => (
-  <ThemeProvider theme={Theme}>{element}</ThemeProvider>
+  <ThemeProvider theme={Theme}>
+    <GlobalStyles />
+    {element}
+  </ThemeProvider>
 )
